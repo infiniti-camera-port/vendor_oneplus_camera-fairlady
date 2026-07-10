6fa998d9de8dd18a01bed7e39d6dfed0bbd82938
1vivy <1vivy@tutanota.com>
2026-07-10 04:30:41 +0000

    camera-fairlady: report stock OTA version
    Change-Id: If6a1cdf8b024520b7f65fffc4c41df9da22c0d44

--- camera-fairlady.mk
+++ camera-fairlady.mk
@@ -6,3 +6,6 @@
 $(call inherit-product, vendor/oneplus/camera-sm8850-common/camera-sm8850-common.mk)
 
 $(call inherit-product, proprietary/vendor/oneplus/camera-fairlady/camera-fairlady-vendor.mk)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.version.ota=PLZ110_11.A.31_0310_202605280615
